5b3f28fe34a1870105954776386320f929f7db43
nickchahley <nickchahley@gmail.com>
2024-05-22 11:08:02 -0400

    Mining split data complete.
    DVC pipeline defined.
    
    Will run `dvc push` after this. Hope to see whether the data outs
    defined in dvc.yaml will go to remote storage or not.

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Data
+```
+string
+	physical.links
+	idmap (w/ HCNG symbol??)
+zhong
+	s4-plasma_protein_profiling.csv
+		subject. id, visit, <vars> 
+		vars ids are in symbol
+		taken from SI mmc6.xlsx
+		this is the expression data input
+	s6-diff_proteins.csv
+		used in freq analysis to get the significant DEPs
+	protein_uniprot_description.csv
+		this MAY still get used in the freq analysis pipe
+
+```
---
+++ src/clean_expression.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import polars as pl
+import polars.selectors as cs
+import argparse, sys
+from motif_mining.utils import lowercase_cols
+
+def cline(args_ls = sys.argv[1:]):
+    p = argparse.ArgumentParser(
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+        description = '',
+    )
+    p.add_argument('infile', help = 'path to expression file')
+    p.add_argument('outfile', help = 'path to write out')
+    p.add_argument('--meta_out', help = 'write metadata table to path, if supplied')
+    p.add_argument('--obs_id', default='id_visit', help = '')
+    p.add_argument('--group_id', default='visit')
+    p.add_argument('--subject_id', default='subject_id')
+    p.add_argument('--var_id', default='symbol', help = '')
+    p.add_argument('--value', default='npx', help = '')
+    args = p.parse_args(args_ls)
+    return args
+
+def main(args):
+    id_cols = [args.subject_id, args.group_id]
+    npx = pl.read_csv(args.infile, null_values=['NA', 'na', 'none', 'None'])
+    npx = npx.rename({x: x.lower().replace(' ', '_') for x in npx.columns})
+    npx = npx.cast({col: pl.Utf8 for col in id_cols})
+    npx = npx.melt(id_vars=id_cols, variable_name=args.var_id, value_name=args.value)
+    npx = npx.with_columns(
+        pl.concat_str( id_cols, separator='_',).alias(args.obs_id),
+        pl.col(args.var_id).str.to_uppercase() # Symbol (also all var_ids, off the top of my head) expect UPPER
+    )
+    npx.write_csv(args.outfile)
+
+    if args.meta_out:
+        meta = npx.select(pl.exclude([args.value, args.var_id])).unique()
+        meta.write_csv(args.meta_out)
+
+if __name__ == "__main__":
+    main(cline())
