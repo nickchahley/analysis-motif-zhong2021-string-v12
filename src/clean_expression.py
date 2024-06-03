7d3e90b49a908aecd531f7f71e9a1aa8cb76935b
nickchahley <nickchahley@gmail.com>
2024-06-03 12:12:37 -0400

    feat: include filter by ppi graph into pipeline
    Filter by ppi graph: exclude any patters which are not found to be
    directly connected on in the PPI graph

--- src/clean_expression.py
+++ src/clean_expression.py
@@ -2,7 +2,7 @@
 import polars as pl
 import polars.selectors as cs
 import argparse, sys
-from motif_mining.utils import lowercase_cols
+from motif_mining.utils.misc import lowercase_cols
 
 def cline(args_ls = sys.argv[1:]):
     p = argparse.ArgumentParser(
@@ -16,6 +16,7 @@ def cline(args_ls = sys.argv[1:]):
                    help = 'Create obs_id column of this name by joining group and subject id cols')
     p.add_argument('--obs_id_sep', default='_', help = 'Separator to use in creating obs_id')
     p.add_argument('--group_id', default='visit')
+    p.add_argument('--rename_group_id', action='store_true', help = 'If supplied, rename group_id to "group"')
     p.add_argument('--subject_id', default='subject_id')
     p.add_argument('--var_id', default='symbol', help = '')
     p.add_argument('--value', default='npx', help = '')
@@ -26,6 +27,9 @@ def main(args):
     id_cols = [args.subject_id, args.group_id]
     npx = pl.read_csv(args.infile, null_values=['NA', 'na', 'none', 'None'])
     npx = npx.rename({x: x.lower().replace(' ', '_') for x in npx.columns})
+    if args.rename_group_id:
+        npx = npx.rename({args.group_id: 'group'})
+        id_cols = [x.replace(args.group_id, 'group') for x in id_cols]
     npx = npx.cast({col: pl.Utf8 for col in id_cols})
     npx = npx.melt(id_vars=id_cols, variable_name=args.var_id, value_name=args.value)
     npx = npx.with_columns(
